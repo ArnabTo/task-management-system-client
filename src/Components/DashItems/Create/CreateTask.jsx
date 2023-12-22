45effb8ce3a98f2d497ee86df2e1daeada9bb8fd
ArnabTo <arnabs929@gmail.com>
2023-12-22 13:45:46 +0600

    create task added in dashboard

--- src/Components/DashItems/Create/CreateTask.jsx
+++ src/Components/DashItems/Create/CreateTask.jsx
@@ -1,8 +1,34 @@
+import axios from 'axios';
+import { Label, Textarea } from 'flowbite-react';
+import { useForm } from "react-hook-form"
 const CreateTask = () => {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm()
+    
+      const onSubmit = (data) => {
+        console.log(data)
+    
+        axios.post('http://localhost:5000/tasks', data)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }
+    
     return (
         <div>
             <h2 className="text-3xl text-center font-bold my-8">Create Your Task Here</h2>
- 
+            <div className="max-w-md mx-auto">
+                <div className="mb-2 block">
+                    <Label htmlFor="comment" value="Your message" />
+                </div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Textarea id="comment" {...register('task')} placeholder="Leave a comment..." required rows={4} />
+                    <button type='submit' className='bg-white text-black my-4 rounded px-8 py-2 uppercase'>Add</button>
+                </form>
+            </div>
         </div>
     );
 };
