e77429243ce1767cbc8a2ab4b8f63c955964854e
ArnabTo <arnabs929@gmail.com>
2024-01-07 21:48:41 +0600

    added smooth scroll

---
+++ src/Components/SmoothScroll.jsx
@@ -0,0 +1,11 @@
+import { ReactLenis, useLenis } from '@studio-freight/react-lenis'
+
+function SmoothScroll({children}) {
+  return (
+    <ReactLenis root>
+      {children}
+    </ReactLenis>
+  )
+}
+
+export default SmoothScroll;
--- src/main.jsx
+++ src/main.jsx
@@ -10,17 +10,20 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import SmoothScroll from './Components/SmoothScroll';
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <DndProvider backend={HTML5Backend}>
+  <SmoothScroll>
     <React.StrictMode>
       <AuthProvider>
         <QueryClientProvider client={queryClient}>
           <RouterProvider router={router} />
         </QueryClientProvider>
       </AuthProvider>
-    </React.StrictMode>,
+    </React.StrictMode>
+    </SmoothScroll>
   </DndProvider>
 )
